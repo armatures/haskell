05a2104cf254cb5dd19f035f44963a5c96f9a30f
Charlie Bevis <charliebevis@users.noreply.github.com>
2014-09-13 21:11:49 -0700

    There are some pretty magnificent functions in here that you might want

--- functions.hs
+++ functions.hs
@@ -1,7 +1,21 @@
-intersperse :: t -> [[t]] -> [t]
-intersperse _ [] = []
-intersperse seperator [a] = a
-intersperse seperator (x:xs) = x ++ [seperator] ++ (intersperse seperator (xs))
+import Data.List
+palindrome [] = []
+palindrome(x:xs) = x:palindrome(xs) ++ [x]
 
-intersperseExample = intersperse ' ' ["check","it","out"]
+isPalindrome [] = True
+isPalindrome [_] = True
+isPalindrome(x:xs) = (firstLast && middle)
+  where firstLast = x == last(xs)
+        middle = isPalindrome(init(xs))
 
+byLength :: [a] -> [a1] -> Ordering
+byLength a b = compare (length a) (length b)
+sortByLength x = sortBy byLength x
+sortByLengthExample = sortByLength ["third","1st","2nd!"]
+
+intersperse' :: t -> [[t]] -> [t]
+intersperse' _ [] = []
+intersperse' seperator [a] = a
+intersperse' seperator (x:xs) = x ++ [seperator] ++ (intersperse' seperator (xs))
+
+intersperse'Example = intersperse' ' ' ["check","it","out"]
