fa7457feedf8f6f9e2746bad25dd87ef4a583ec0
Charlie Bevis <charliebevis@users.noreply.github.com>
2014-09-15 07:53:08 -0700

    Two tree types
    - one uses Maybes for every Node, the other uses Empty for Empty trees.

---
+++ tree.hs
@@ -0,0 +1,7 @@
+data Tree a = Node a (Maybe (Tree a)) ( Maybe (Tree a))
+                   deriving (Show)
+
+data Tree' a = Node' a (Tree' a) (Tree' a) 
+               | Empty
+               deriving(Show)
+
