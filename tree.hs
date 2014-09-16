fb6fba8b4b46d51d7e7f9b07c748dff8ebf4939f
Charlie Bevis <charliebevis@users.noreply.github.com>
2014-09-15 21:59:22 -0700

    calculates the height of a binary tree

--- tree.hs
+++ tree.hs
@@ -1,7 +1,14 @@
-data Tree a = Node a (Maybe (Tree a)) ( Maybe (Tree a))
+data Tree' a = Node' a (Maybe (Tree' a)) ( Maybe (Tree' a))
                    deriving (Show)
 
-data Tree' a = Node' a (Tree' a) (Tree' a) 
+data Tree a = Node a (Tree a) (Tree a) 
                | Empty
                deriving(Show)
 
+height :: Tree a -> Int
+height (Empty) = 0
+height (Node _ l r)
+    | left < right = 1 + right
+    | otherwise = 1 + left
+      where left = height l
+            right = height r
