6894d0fabd1299a0c0b1080a5b986c17ad786aa7
Charlie Bevis <charliebevis@users.noreply.github.com>
2014-09-17 22:12:54 -0700

    Do 3 points constitute a left or right turn? Are they colinear?

---
+++ plane.hs
@@ -0,0 +1,8 @@
+data Direction = Lturn | Straight | Rturn
+                 deriving (Show)
+
+turn :: (Num a, Ord a) => (a, a) -> (a, a) -> (a, a) -> Direction
+turn (x1,y1) (x2,y2) (x3,y3) | cross > 0  = Lturn
+                             | cross == 0 = Straight
+                             | cross < 0  = Rturn
+                               where cross = (x2 - x1)*(y3 - y1) - (y2 - y1)*(x3 - x1)
