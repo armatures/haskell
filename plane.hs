69b94140b2d9bd33b5a1e419faa16fcd0e87e691
Charlie Bevis <charliebevis@users.noreply.github.com>
2014-09-19 07:51:06 -0700

    Calculate all the turns involved in connecting a series of points

--- plane.hs
+++ plane.hs
@@ -6,3 +6,6 @@ turn (x1,y1) (x2,y2) (x3,y3) | cross > 0  = Lturn
                              | cross == 0 = Straight
                              | cross < 0  = Rturn
                                where cross = (x2 - x1)*(y3 - y1) - (y2 - y1)*(x3 - x1)
+
+triples x1:x2:x3:xs = (turn x1 x2 x3):triples(x2:x3:xs)
+triples _ = []
