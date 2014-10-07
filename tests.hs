ba181f9cbddae64b38f2b311d29d684c200ce32e
Charlie Bevis <charliebevis@users.noreply.github.com>
2014-10-06 22:55:42 -0700

    tests for subsequent Graham scan functions

--- tests.hs
+++ tests.hs
@@ -7,3 +7,6 @@ groupByYTest = TestCase $ assertEqual "groups" [[(0,2)], [ (2,1), (1, 1)]] (grou
 firstTest = TestCase $ assertEqual "takes the point with the smallest y-coordinate" (1,-10) (firstPoint [(0,0), (1,-10), (-1, 1)])
 firstTest1 = TestCase $ assertEqual "in a tie, takes the point with the smallest x-coordinate" (1,1) (firstPoint [(0,2), (2,1), (1, 1)])
 runFirstTests = runTestTT $ TestList [firstTest, firstTest1]
+
+sortByPolarAngleTest = TestCase $ assertEqual "orders the points by polar angle with the x-axis and the bottom-left point" [(1,-1),(1,1),(-1,1)] (sortByPolarAngle [(-1,-1),(-1,1),(1,1),(1,-1)])
+otherPointsTest = TestCase $ assertEqual "all the points that aren't the first point" [(1,-1),(1,1),(-1,1)] (otherPoints [(1,-1),(-1,-1),(1,1),(-1,1)])
